380b894a545cfa12528ff3c844b43899aa7b6758
kevarvizu <kevinarvizu@hotmail.com>
2016-12-13 17:25:30 -0700

    modificacion clase carrera
    agregué otros metodos que encontré pero usan arreglos, solo me falta la implementacion para archivos.

--- beans/Carrera.java
+++ beans/Carrera.java
@@ -1,5 +1,6 @@
 package beans;
 
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -7,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
 /**
  * Created by atempa on 12/12/16.
  */
@@ -39,15 +40,7 @@ public class Carrera {
         this.nombre = nombre;
     }
 
-    @Override
-    public String toString() {
-        return "Carrera{" +
-                "id_carrera=" + id_carrera +
-                ", nombre='" + nombre + '\'' +
-                '}';
-    }
-    
-     public  void Escribir() throws IOException {
+    public  void Escribir() throws IOException {
         String ruta = "C:/Users/Kev/Desktop/Test-master/archivo.txt";
         File archivo = new File(ruta);
         BufferedWriter bw;
@@ -69,4 +62,96 @@ public class Carrera {
         }
         b.close();
     } 
+    int p=0, pos=-1;
+    
+    public void registrar(Alumno[] em , int x)throws IOException{
+   	 
+       BufferedReader i= new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("\n\n\nId del alumno: ");
+       int id=Integer.parseInt(i.readLine()); 
+       System.out.println("\nNombre del alumno:  ");
+       String no=i.readLine();
+       System.out.println("\nApellido Paterno: ");
+       String ap=i.readLine();
+       System.out.println("\nApellido Materno: ");
+       String am=i.readLine();
+       System.out.println("Numero de control: ");
+       String con =i.readLine();
+       System.out.println("Carrera: ");
+       String car=i.readLine();
+  
+       em[x]= new Alumno(id,no,ap,am,con,car);
+    
+       p=x;
+    }
+    
+    public int bajas(Alumno[] empZ, int x) throws IOException{
+        BufferedReader k = new BufferedReader(new InputStreamReader(System.in));
+        int h = 0;
+        System.out.println("Introduzca el nombre:");
+        String baja = k.readLine();
+        for(int y=0;y<x;y++){
+            String nomb = empZ[y].getNombre();
+            if(nomb.equalsIgnoreCase(baja)){
+                h = y;
+                System.out.println(empZ[h].getNombre());
+                System.out.println("Desea darlo de baja? (si/no):");
+                String r = k.readLine();
+                if(r.equalsIgnoreCase("si"))
+                    for(int a=h;a<x;a++)
+                        empZ[a]=empZ[a+1];
+                x--;
+                p--;
+                y=x;
+                if(r.equalsIgnoreCase("no"))
+                    System.out.println("No se eliminara");
+            }
+        }
+        return x;
+    }
+    public void modificaciones(Alumno[] e6, int x)throws IOException{
+        BufferedReader in= new BufferedReader(new InputStreamReader(System.in));   
+        int ll=0;
+        String desc="NULL";
+              System.out.println("\nIngresa nuevo nombre: ");
+              String busca= in.readLine();
+              for(int i=0;i<x;i++){
+                 desc=e6[i].getNombre();
+                  if(desc.equalsIgnoreCase(busca)){
+                         ll=i;
+                  }
+              }
+              System.out.println("Ingresa nuevo apellido paterno: ");
+              String busca2= in.readLine();
+              for(int i=0;i<x;i++){
+                 desc=e6[i].getNombre();
+                  if(desc.equalsIgnoreCase(busca2)){
+                         ll=i;
+}
+   }
+              System.out.println("Ingresa nuevo apellido materno: ");
+              String busca3= in.readLine();
+              for(int i=0;i<x;i++){
+                 desc=e6[i].getNombre();
+                  if(desc.equalsIgnoreCase(busca3)){
+                         ll=i;
+}
+}
+              System.out.println("Ingresa nuevo numero de control: ");
+              String busca4= in.readLine();
+              for(int i=0;i<x;i++){
+                 desc=e6[i].getNombre();
+                  if(desc.equalsIgnoreCase(busca4)){
+                         ll=i;
+}
+}
+              System.out.println("Ingresa nueva carrera: ");
+              String busca5= in.readLine();
+              for(int i=0;i<x;i++){
+                 desc=e6[i].getNombre();
+                  if(desc.equalsIgnoreCase(busca5)){
+                         ll=i;
+}
+}
+    }
 }
